47dc1b2f48fa32c312792095ca6577d702cc3f64
brittanieb <brittanie.boyko@gmail.com>
2020-02-29 22:21:39 -0500

    added check for emply value in add burger text area

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,37 +1,40 @@
 $(function() {
-    $(".eat-burger").on("click", function(event) {
+  $(".eat-burger").on("click", function(event) {
       let id = $(this).data("id");
       let devouredState = $(this).data("devoured");
-  
+
       let newBurgerState = {
-        devoured: devouredState
+          devoured: devouredState
       };
 
       $.ajax("/api/burgers/" + id, {
-        type: "PUT",
-        data: newBurgerState
+          type: "PUT",
+          data: newBurgerState
       }).then(
-        function() {
-          location.reload();
-        }
-      );
-    });
-
-    $(".create-form").on("submit", function(event) {
-        event.preventDefault();
-    
-        let newBurger = {
-          burger_name: $("#ca").val().trim(),
-          devoured: 0
-        };
-    
-        $.ajax("/api/burgers", {
-          type: "POST",
-          data: newBurger
-        }).then(
           function() {
-            location.reload();
+              location.reload();
           }
-        );
-      });
+      );
+  });
+
+  $(".create-form").on("submit", function(event) {
+      event.preventDefault();
+      
+      let burgerValue = $("#ca").val().trim();
+      if (burgerValue) {
+          let newBurger = {
+              burger_name: $("#ca").val().trim(),
+              devoured: 0
+          };
+
+          $.ajax("/api/burgers", {
+              type: "POST",
+              data: newBurger
+          }).then(
+              function() {
+                  location.reload();
+              }
+          );
+      }
+  });
 })
